cd6f2967a9f08dbc1f8942d97144f1ffb2a22a3c
Nikki Harper <nicoleharper@ufl.edu>
2021-03-20 11:32:19 -0600

    Display appropriate water temp message after 3 seconds

--- main.js
+++ main.js
@@ -13,5 +13,29 @@ const waterTemp = document.getElementById("water-temp");
 const displayWaterTemp = () => waterTemp.innerHTML = "80";
 displayWaterTemp();
 
+//Display appropriate message after 3 seconds based on water temp 
+const boiling = document.getElementById("boiling");
+const frozen = document.getElementById("frozen"); 
+const goodTemp = document.getElementById("good-temp");
+const waterTempNumber = document.getElementById("water-temp").innerHTML; 
 
+const displayMessage = () =>
+    {
+        if (waterTempNumber > 212) {
+            boiling.classList.remove("hide");
+            boiling.classList.add("show");
+        }
+
+        if (waterTempNumber < 32) {
+            frozen.classList.remove("hide");
+            frozen.classList.add("show");
+        }
+
+        if (waterTempNumber >= 32 && waterTempNumber <=212) {
+            goodTemp.classList.remove("hide");
+            goodTemp.classList.add("show");
+        }
+    }; 
+
+setTimeout(displayMessage, 3000); 
 
