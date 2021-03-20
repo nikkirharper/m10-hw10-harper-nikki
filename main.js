c641ad9a3e19d54e5278b2c876d750083c2de058
Nikki Harper <nicoleharper@ufl.edu>
2021-03-20 10:35:39 -0600

    Add boiling temp, freezing temp and water temp

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// It is always helpful to use comments in your code!
+// Immediately print 112 as boiling temp
+const boilingTemp = document.getElementById("boiling-temp"); 
+const displayBoilingTemp = () => boilingTemp.innerHTML = "212";
+displayBoilingTemp(); 
+
+// Immediately print 32 as freezing temp
+const freezingTemp = document.getElementById("freezing-temp"); 
+const displayFreezingTemp = () => freezingTemp.innerHTML = "32";
+displayFreezingTemp();
+
+// Immediately print 80 as water
+const waterTemp = document.getElementById("water-temp"); 
+const displayWaterTemp = () => waterTemp.innerHTML = "80";
+displayWaterTemp();
+
+
+
